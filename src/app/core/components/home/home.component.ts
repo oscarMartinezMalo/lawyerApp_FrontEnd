71ab3539b0b7d2e5b377a3eacb61bd2f1bc5b5be
Oscar Martinez Malo <omartinez@onehomecaresolutions.net>
2021-11-25 13:20:21 -0500

    Working in home Template

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+
+export class HomeComponent implements OnInit {  
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener("document:scroll", []) onWindowScroll() {
+    console.log("scrollActivated");
+  }
+
+
+  onScrollClick(scrollElement, valueToScroll){
+    scrollElement.scrollTo({ left: ( valueToScroll), behavior: 'smooth' });
+  }
 }
